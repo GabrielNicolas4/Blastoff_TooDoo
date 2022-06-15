7a2e63cd31428d9517463bfc4f9ce0becfccef44
GabrielNicolas4 <107160865+GabrielNicolas4@users.noreply.github.com>
2022-06-15 01:09:16 -0300

    Add files via upload

---
+++ Questão 11.js
@@ -0,0 +1,5 @@
+
+multiplica(4,7) = 28
+multiplica(5,6) = 30
+multiplica(3,3) = 9
+ C - !important
---
+++ Questão 12.js
@@ -0,0 +1,18 @@
+li.list-item {
+    color: red;
+    }
+    .list-item {
+    color: blue
+    }
+    #list .list-item {
+    color: green
+    }
+    ul li {
+    color: black;
+    }
+    (a) red
+    (b) blue
+    (c) green
+    (d) black
+
+    Letra C pela especificação= Green (C)
